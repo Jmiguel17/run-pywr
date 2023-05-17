28521917914656fe0cd6273ae8cc06d1b0af553a
Jose Miguel Gonzalez Cabrera <jose.gonzalez18@outlook.com>
2023-05-17 19:00:47 +0100

    adding recorders

--- run_pywr/custom_parameters.py
+++ run_pywr/custom_parameters.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from pywr.nodes import Storage
 from scipy.interpolate import Rbf
 from pywr.parameter_property import parameter_property
@@ -214,3 +215,156 @@ class IrrigationWaterRequirementParameter(Parameter):
 
 
 IrrigationWaterRequirementParameter.register()
+
+
+class TransientDecisionParameter(Parameter):
+    """ Return one of two values depending on the current time-step
+
+    This `Parameter` can be used to model a discrete decision event
+     that happens at a given date. Prior to this date the `before`
+     value is returned, and post this date the `after` value is returned.
+
+    Parameters
+    ----------
+    decision_date : string or pandas.Timestamp
+        The trigger date for the decision.
+    before_parameter : Parameter
+        The value to use before the decision date.
+    after_parameter : Parameter
+        The value to use after the decision date.
+    earliest_date : string or pandas.Timestamp or None
+        Earliest date that the variable can be set to. Defaults to `model.timestepper.start`
+    latest_date : string or pandas.Timestamp or None
+        Latest date that the variable can be set to. Defaults to `model.timestepper.end`
+    decision_freq : pandas frequency string (default 'AS')
+        The resolution of feasible dates. For example 'AS' would create feasible dates every
+        year between `earliest_date` and `latest_date`. The `pandas` functions are used
+        internally for delta date calculations.
+
+    """
+
+    def __init__(self, model, decision_date, before_parameter, after_parameter,
+                 earliest_date=None, latest_date=None, decision_freq='AS', **kwargs):
+        super(TransientDecisionParameter, self).__init__(model, **kwargs)
+        self._decision_date = None
+        self.decision_date = decision_date
+
+        if not isinstance(before_parameter, Parameter):
+            raise ValueError('The `before` value should be a Parameter instance.')
+        before_parameter.parents.add(self)
+        self.before_parameter = before_parameter
+
+        if not isinstance(after_parameter, Parameter):
+            raise ValueError('The `after` value should be a Parameter instance.')
+        after_parameter.parents.add(self)
+        self.after_parameter = after_parameter
+
+        # These parameters are mostly used if this class is used as variable.
+        self._earliest_date = None
+        self.earliest_date = earliest_date
+
+        self._latest_date = None
+        self.latest_date = latest_date
+
+        self.decision_freq = decision_freq
+        self._feasible_dates = None
+        self.integer_size = 1  # This parameter has a single integer variable
+
+    def decision_date():
+        def fget(self):
+            return self._decision_date
+
+        def fset(self, value):
+            if isinstance(value, pd.Timestamp):
+                self._decision_date = value
+            else:
+                self._decision_date = pd.to_datetime(value)
+
+        return locals()
+
+    decision_date = property(**decision_date())
+
+    def earliest_date():
+        def fget(self):
+            if self._earliest_date is not None:
+                return self._earliest_date
+            else:
+                return self.model.timestepper.start
+
+        def fset(self, value):
+            if isinstance(value, pd.Timestamp):
+                self._earliest_date = value
+            else:
+                self._earliest_date = pd.to_datetime(value)
+
+        return locals()
+
+    earliest_date = property(**earliest_date())
+
+    def latest_date():
+        def fget(self):
+            if self._latest_date is not None:
+                return self._latest_date
+            else:
+                return self.model.timestepper.end
+
+        def fset(self, value):
+            if isinstance(value, pd.Timestamp):
+                self._latest_date = value
+            else:
+                self._latest_date = pd.to_datetime(value)
+
+        return locals()
+
+    latest_date = property(**latest_date())
+
+    def setup(self):
+        super(TransientDecisionParameter, self).setup()
+
+        # Now setup the feasible dates for when this object is used as a variable.
+        self._feasible_dates = pd.date_range(self.earliest_date, self.latest_date,
+                                                 freq=self.decision_freq)
+
+    def value(self, ts, scenario_index):
+
+        if ts is None:
+            v = self.before_parameter.get_value(scenario_index)
+        elif ts.datetime >= self.decision_date:
+            v = self.after_parameter.get_value(scenario_index)
+        else:
+            v = self.before_parameter.get_value(scenario_index)
+        return v
+
+    def get_integer_lower_bounds(self):
+        return np.array([0, ], dtype=np.int)
+
+    def get_integer_upper_bounds(self):
+        return np.array([len(self._feasible_dates) - 1, ], dtype=np.int)
+
+    def set_integer_variables(self, values):
+        # Update the decision date with the corresponding feasible date
+        self.decision_date = self._feasible_dates[values[0]]
+
+    def get_integer_variables(self):
+        return np.array([self._feasible_dates.get_loc(self.decision_date), ], dtype=np.int)
+
+    def dump(self):
+
+        data = {
+            'earliest_date': self.earliest_date.isoformat(),
+            'latest_date': self.latest_date.isoformat(),
+            'decision_date': self.decision_date.isoformat(),
+            'decision_frequency': self.decision_freq
+        }
+
+        return data
+
+    @classmethod
+    def load(cls, model, data):
+
+        before_parameter = load_parameter(model, data.pop('before_parameter'))
+        after_parameter = load_parameter(model, data.pop('after_parameter'))
+
+        return cls(model, before_parameter=before_parameter, after_parameter=after_parameter, **data)
+        
+TransientDecisionParameter.register()
--- run_pywr/custom_recorders.py
+++ run_pywr/custom_recorders.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 
-from pywr.parameters import load_parameter
-from pywr.recorders import MeanParameterRecorder, NumpyArrayNodeRecorder, NodeRecorder, Aggregator, BaseConstantParameterRecorder
+#from pywr.parameters import load_parameter
+from pywr.recorders import NumpyArrayNodeRecorder, NodeRecorder, Aggregator, NumpyArrayStorageRecorder
 
 
 class NumpyArrayAnnualNodeDeficitFrequencyRecorder(NodeRecorder):
@@ -85,3 +85,240 @@ class NumpyArrayAnnualNodeDeficitFrequencyRecorder(NodeRecorder):
 
 
 NumpyArrayAnnualNodeDeficitFrequencyRecorder.register()
+
+
+class AbstractComparisonNodeRecorder(NumpyArrayNodeRecorder):
+    """ Base class for all Recorders performing timeseries comparison of `Node` flows
+    """
+
+    def __init__(self, model, node, observed, obs_freq=None, **kwargs):
+        super(AbstractComparisonNodeRecorder, self).__init__(model, node, **kwargs)
+
+        self.observed = observed
+        self._aligned_observed = None
+        self.obs_freq = obs_freq
+
+    def setup(self):
+        super(AbstractComparisonNodeRecorder, self).setup()
+        # Align the observed data to the model
+
+        from pywr.parameters import align_and_resample_dataframe
+
+        freq = self.obs_freq
+        index_col = self.observed.index.tolist()
+        start, end = index_col[0], index_col[-1]
+        timestepper = pd.period_range(start=start, end=end, freq=freq)
+        self._aligned_observed = align_and_resample_dataframe(self.observed, timestepper, 'sum')
+        #self._aligned_observed = align_and_resample_dataframe(self.observed, self.model.timestepper.datetime_index)
+
+    @classmethod
+    def load(cls, model, data):
+        # called when the parameter is loaded from a JSON document
+
+        observed = data.pop("observed")
+        index_col = data.pop("index_col")
+        url = data.pop("url")
+        obs_freq = data.pop("obs_freq")
+
+        if '.csv' in url:
+            data_observed = pd.read_csv(url)
+
+        if 'xlsx' in url:
+            data_observed = pd.read_excel(url)
+
+        observed = pd.DataFrame(data=np.array(data_observed[observed]), index=data_observed[index_col])
+
+        node = model._get_node_from_ref(model, data.pop("node"))
+
+        return cls(model, node, observed, obs_freq, **data)
+
+
+class RootMeanSquaredErrorNodeRecorder(AbstractComparisonNodeRecorder):
+    """ Recorder evaluates the RMSE between model and observed """
+    def values(self):
+
+        freq = self.obs_freq
+        obs = self._aligned_observed
+        mod = self.data
+
+        if freq is None:
+            mod = self.data
+        else:
+            if self.model.timestepper.freq == freq:
+                mod = pd.DataFrame(self.data, index=self.model.timestepper.datetime_index).resample(freq).mean()
+            else:
+                #print(f'OJO! The recorder associated to this node "{self.node.name}" '
+                      #f'has freq observed data =! freq model - '
+                      #f'Check if freq observed data >= freq model.')
+                mod = pd.DataFrame(self.data, index=self.model.timestepper.datetime_index.astype('datetime64[ns]')).resample(freq).mean()
+                mod.index = mod.index.strftime('%Y-%m')
+                obs.index = obs.index.astype('datetime64[ns]').strftime('%Y-%m')
+
+            #mod = pandas.DataFrame(self.data, index=self.model.timestepper.datetime_index).resample(freq).sum()
+
+        new = pd.merge(obs, mod, how='inner', left_index=True, right_index=True)
+        obs = new.iloc[:, 0].to_frame().T.reset_index(drop=True).T
+        mod = new.iloc[:, 1].to_frame().T.reset_index(drop=True).T
+
+        val = np.sqrt(np.mean((obs - mod) ** 2, axis=0))
+
+        return val.values
+
+
+RootMeanSquaredErrorNodeRecorder.register()
+
+
+class NashSutcliffeEfficiencyNodeRecorder(AbstractComparisonNodeRecorder):
+    """ Recorder evaluates the Nash-Sutcliffe efficiency model and observed """
+    def values(self):
+
+        freq = self.obs_freq
+        obs = self._aligned_observed
+
+        mod = self.data
+
+        if freq is None:
+            mod = self.data
+        else:
+            if self.model.timestepper.freq == freq:
+                mod = pd.DataFrame(self.data, index=self.model.timestepper.datetime_index).resample(freq).mean()
+            else:
+                #print(f'OJO! The recorder associated to this node "{self.node.name}" '
+                      #f'has freq observed data =! freq model - '
+                      #f'Check if freq observed data >= freq model.')
+                mod = pd.DataFrame(self.data, index=self.model.timestepper.datetime_index.astype('datetime64[ns]')).resample(freq).mean()
+                mod.index = mod.index.strftime('%Y-%m')
+                obs.index = obs.index.astype('datetime64[ns]').strftime('%Y-%m')
+
+            #mod = pandas.DataFrame(self.data, index=self.model.timestepper.datetime_index).resample(freq).sum()
+
+        new = pd.merge(obs,mod, how='inner', left_index=True, right_index=True)
+        obs = new.iloc[:, 0].to_frame().T.reset_index(drop=True).T
+        mod = new.iloc[:, 1].to_frame().T.reset_index(drop=True).T
+
+        obs_mean = np.mean(obs, axis=0)
+
+        val = 1.0 - np.sum((obs-mod)**2, axis=0)/np.sum((obs-obs_mean)**2, axis=0)
+
+        return val.values
+
+
+NashSutcliffeEfficiencyNodeRecorder.register()
+
+
+class PercentBiasNodeRecorder(AbstractComparisonNodeRecorder):
+    """ Recorder evaluates the percent bias between model and observed """
+    def values(self):
+
+        freq = self.obs_freq
+        obs = self._aligned_observed
+        mod = self.data
+
+        if freq is None:
+            mod = self.data
+        else:
+            if self.model.timestepper.freq == freq:
+                mod = pd.DataFrame(self.data, index=self.model.timestepper.datetime_index).resample(freq).mean()
+            else:
+                #print(f'OJO! The recorder associated to this node "{self.node.name}" '
+                      #f'has freq observed data =! freq model - '
+                      #f'Check if freq observed data >= freq model.')
+                mod = pd.DataFrame(self.data, index=self.model.timestepper.datetime_index.astype('datetime64[ns]')).resample(freq).mean()
+                mod.index = mod.index.strftime('%Y-%m')
+                obs.index = obs.index.astype('datetime64[ns]').strftime('%Y-%m')
+
+            #mod = pandas.DataFrame(self.data, index=self.model.timestepper.datetime_index).resample(freq).sum()
+
+        new = pd.merge(obs,mod, how='inner', left_index=True, right_index=True)
+        obs = new.iloc[:, 0].to_frame().T.reset_index(drop=True).T
+        mod = new.iloc[:, 1].to_frame().T.reset_index(drop=True).T
+
+        val = np.sum(obs-mod, axis=0)*100/np.sum(obs, axis=0)
+
+        return val.values
+
+
+PercentBiasNodeRecorder.register()
+
+
+class AbstractComparisonStorageRecorder(NumpyArrayStorageRecorder):
+    """ Base class for all Recorders performing timeseries comparison of `Storage Node`
+    """
+
+    def __init__(self, model, node, observed, obs_freq=None, **kwargs):
+        super(AbstractComparisonStorageRecorder, self).__init__(model, node, **kwargs)
+
+        self.observed = observed
+        self._aligned_observed = None
+        self.obs_freq = obs_freq
+
+    def setup(self):
+        super(AbstractComparisonStorageRecorder, self).setup()
+        # Align the observed data to the model
+
+        from pywr.parameters import align_and_resample_dataframe
+        #        self._aligned_observed = align_and_resample_dataframe(self.observed, self.model.timestepper.datetime_index)
+
+        freq = self.obs_freq
+        index_col = self.observed.index.tolist()
+        start, end = index_col[0], index_col[-1]
+        timestepper = pd.period_range(start=start, end=end, freq=freq)
+        self._aligned_observed = align_and_resample_dataframe(self.observed, timestepper, 'mean')
+
+    @classmethod
+    def load(cls, model, data):
+        # called when the parameter is loaded from a JSON document
+
+        observed = data.pop("observed")
+        index_col = data.pop("index_col")
+        url = data.pop("url")
+        obs_freq = data.pop("obs_freq")
+
+        if '.csv' in url:
+            data_observed = pd.read_csv(url)
+
+        if 'xlsx' in url:
+            data_observed = pd.read_excel(url)
+
+        observed = pd.DataFrame(data=np.array(data_observed[observed]), index=data_observed[index_col])
+
+        node = model._get_node_from_ref(model, data.pop("node"))
+
+        return cls(model, node, observed, obs_freq, **data)
+
+
+class NashSutcliffeEfficiencyStorageRecorder(AbstractComparisonStorageRecorder):
+    """ Recorder evaluates the Nash-Sutcliffe efficiency model and observed """
+
+    def values(self):
+
+        freq = self.obs_freq
+        obs = self._aligned_observed
+
+        mod = self.data
+
+        if freq is None:
+            mod = self.data
+        else:
+            if self.model.timestepper.freq == freq:
+                mod = pd.DataFrame(self.data, index=self.model.timestepper.datetime_index).resample(freq).mean()
+            else:
+                #print(f'OJO! The recorder associated to this node "{self.node.name}" '
+                      #f'has freq observed data =! freq model - '
+                      #f'Check if freq observed data >= freq model')
+                mod = pd.DataFrame(self.data, index=self.model.timestepper.datetime_index.astype('datetime64[ns]')).resample(freq).mean()
+                mod.index = mod.index.strftime('%Y-%m')
+                obs.index = obs.index.astype('datetime64[ns]').strftime('%Y-%m')
+
+        new = pd.merge(obs, mod, how='inner', left_index=True, right_index=True)
+        obs = new.iloc[:, 0].to_frame().T.reset_index(drop=True).T
+        mod = new.iloc[:, 1].to_frame().T.reset_index(drop=True).T
+
+        obs_mean = np.mean(obs, axis=0)
+
+        val = 1.0 - np.sum((obs - mod) ** 2, axis=0) / np.sum((obs - obs_mean) ** 2, axis=0)
+
+        return val.values
+
+
+NashSutcliffeEfficiencyStorageRecorder.register()
